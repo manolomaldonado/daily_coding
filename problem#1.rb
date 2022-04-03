6d5ac951bf529a12d5f9bd06850783d1705ea70e
Manuel Maldonado <manuel.maldonado@lextrend.com>
2022-04-03 19:09:56 +0200

    Solution to problem #1

---
+++ problem#1.rb
@@ -0,0 +1,11 @@
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+# Bonus: Can you do this in one pass?
+
+abort('wrong number of params') unless ARGV.length == 2
+
+p1 = ARGV[0].split(',').map(&:to_i)
+p2 = ARGV[1].to_i
+#TODO: Implement errors control 
+p !p1.permutation(2).to_a.map { |x| x.inject(:+) }.select { |x| x == p2 }.first.nil?
+
